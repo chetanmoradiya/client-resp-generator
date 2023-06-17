2f56a141617ca2b77930317473e5136a5763df54
chetanmoradiya <chetan.moradiya@gmail.com>
2023-06-17 19:31:22 +0530

    first commit

---
+++ src/main/java/com/iris/hackathon/respgenerator/ClientRespGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.respgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRespGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRespGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iris/hackathon/respgenerator/ClientRespGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.respgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRespGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
